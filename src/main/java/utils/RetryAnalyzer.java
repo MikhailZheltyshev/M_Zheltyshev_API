80e8a4d62469112280271e4577929a483a815bd8
mihalichzh_on_mac <mihalich93@bk.ru>
2018-11-23 10:39:54 +0300

    Added pause to test retries

--- src/main/java/utils/RetryAnalyzer.java
+++ src/main/java/utils/RetryAnalyzer.java
@@ -3,6 +3,10 @@ package utils;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
+/**
+    Retry analyzer with pauses to increase tests stability.
+ */
+
 public class RetryAnalyzer implements IRetryAnalyzer {
 
     int counter = 0;
@@ -10,9 +14,15 @@ public class RetryAnalyzer implements IRetryAnalyzer {
 
     @Override
     public boolean retry(ITestResult result) {
-
-        if(counter < retryLimit)
-        {
+        if(counter < retryLimit) {
+            //Pause will be added for further test runs if the first run failed.
+            if (counter > 0) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
             counter++;
             return true;
         }
